c35013b61ed1cf7c920863cf5b0b72676f6ad53f
lilifernandez91 <lilibetfdez@gmail.com>
2023-10-02 18:36:57 +0200

    filters

--- src/main.ts
+++ src/main.ts
@@ -15,13 +15,26 @@ const getData = async (): Promise<Character[]> => {
         const { data } = await axios.get('http://localhost:3000/personajes');
         return data;
     } catch (error) {
-        throw new Error('Error al obtener las películas');
+        console.error('Error al obtener los personajes', error);
+        return [];
     }
 }
 
-const buildCard = (character: Character): HTMLElement => {
+const createTextElement = (label: string, content: string): HTMLElement => {
+    const element = document.createElement('div');
+    element.classList.add('div-name');
+    const labelSpan = document.createElement('span');
+    labelSpan.classList.add('span');
+    labelSpan.textContent = label;
+    const contentSpan = document.createElement('span');
+    contentSpan.textContent = content;
+    element.appendChild(labelSpan);
+    element.appendChild(contentSpan);
+    return element;
+}
 
-    const cardCharacter = document.createElement('div-card');
+const buildCard = (character: Character): HTMLElement => {
+    const cardCharacter = document.createElement('div');
     cardCharacter.classList.add('div-card');
 
     const image = document.createElement('img');
@@ -32,20 +45,9 @@ const buildCard = (character: Character): HTMLElement => {
     const textContainer = document.createElement('div');
     textContainer.classList.add('text-container');
 
-    const name = document.createElement('div');
-    name.classList.add('div-name');
-    name.textContent = `Nombre: ${character.nombre}`;
-    textContainer.appendChild(name);
-
-    const specialty = document.createElement('div')
-    specialty.classList.add('div-specialty');
-    specialty.textContent = `Especialidad: ${character.especialidad}`;
-    textContainer.appendChild(specialty);
-
-    const skills = document.createElement('div');
-    skills.classList.add('div.skills');
-    skills.textContent = `Habilidades: ${character.habilidades.join(',')}`;
-    textContainer.appendChild(skills);
+    textContainer.appendChild(createTextElement('Nombre: ', character.nombre));
+    textContainer.appendChild(createTextElement('Especialidad: ', character.especialidad));
+    textContainer.appendChild(createTextElement('Habilidades: ', character.habilidades.join(', ')));
 
     cardCharacter.appendChild(textContainer);
 
@@ -53,11 +55,11 @@ const buildCard = (character: Character): HTMLElement => {
 }
 
 const containerCard = (characters: Character[]) => {
-
-    const cardsDiv = document.getElementById('cards')
+    const cardsDiv = document.getElementById('cards');
 
     for (const character of characters) {
-        const card = buildCard(character) // retornar el HTML correspondiente a UNA tarjeta
+        const card = buildCard(character)
+
         if (cardsDiv) {
             cardsDiv.appendChild(card);
         }
@@ -66,3 +68,75 @@ const containerCard = (characters: Character[]) => {
 
 const data = await getData()
 containerCard(data)
+
+const removeAccents = (str: string): string => {
+    const accentMap: { [key: string]: string } = {
+        á: 'a',
+        é: 'e',
+        í: 'i',
+        ó: 'o',
+        ú: 'u',
+        ü: 'u',
+    };
+
+    return str.replace(/[áéíóúüÁÉÍÓÚÜ]/g, (matched: string) => {
+        return accentMap[matched];
+    });
+}
+
+const filterHelper = (search: string, chain: string) => {
+    const searchLower = removeAccents(search.toLocaleLowerCase());
+    const candenaLower = removeAccents(chain.toLocaleLowerCase());
+
+    if (candenaLower.indexOf(searchLower) !== -1) {
+        return true;
+    }
+    return false;
+}
+
+const searchInput = document.getElementById('search-character');
+const filterButton = document.getElementById('btn-filter');
+
+const filterByName = () => {
+    const searchValue = (searchInput as HTMLInputElement).value;
+    const filteredCharacters = data.filter(character => filterHelper(searchValue, character.nombre));
+
+    const message = document.getElementById('message')
+    const cardsDiv = document.getElementById('cards');
+
+    if (filteredCharacters.length === 0) {
+        if (message && message instanceof HTMLParagraphElement) {
+            message.textContent = 'No se encontraron personajes con este nombre'
+
+            if (cardsDiv) {
+                while (cardsDiv.firstChild) {
+                    cardsDiv.removeChild(cardsDiv.firstChild);
+                }
+            }
+        }
+    } else {
+        displayFilteredCharacters(filteredCharacters);
+    }
+}
+
+if (filterButton && searchInput) {
+    filterButton.addEventListener('click', filterByName);
+}
+
+const displayFilteredCharacters = (filteredCharacters: Character[]) => {
+    const cardsDiv = document.getElementById('cards');
+
+    if (cardsDiv) {
+        while (cardsDiv.firstChild) {
+            cardsDiv.removeChild(cardsDiv.firstChild);
+        }
+    }
+
+    for (const character of filteredCharacters) {
+        const card = buildCard(character);
+
+        if (cardsDiv) {
+            cardsDiv.appendChild(card)
+        }
+    }
+}
